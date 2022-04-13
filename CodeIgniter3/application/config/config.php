8bc9185bdbdab5b53f9816cb8c5f80707a494859
adityatng <149ty1@gmail.com>
2022-04-13 09:11:48 +0700

    form_validation
    pembuatan validasi untuk memasukkan data kedalam database dengan menggunakan library form_validasi

--- CodeIgniter3/application/config/config.php
+++ CodeIgniter3/application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = '';
-$config['base_url'] = 'http://f7z.adityatng.webprog/pustaka_booking/';
+$config['base_url'] = 'http://localhost/pustaka_booking/';
 
 
 /*
